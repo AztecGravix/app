790f4960e473ae870fa77d25c43c5d57f2bd837c
Pavel Ivanov <sudokey@gmail.com>
2023-10-16 16:01:49 +0300

    Chart layout

---
+++ src/app/components/Chart/TradingView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './index.module.scss'
+
+export const TradingView: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            Chart
+        </div>
+    )
+}
